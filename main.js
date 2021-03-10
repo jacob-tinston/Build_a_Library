758c291adb96dcc6af422d399fd27d9274d9b868
jacob-tinston <jtinston678@gmail.com>
2021-03-10 17:52:28 +0000

    Task Four

--- main.js
+++ main.js
@@ -4,4 +4,16 @@ class Media {
         this._isCheckedOut = false;
         this._ratings = [];
     }
+
+    get title() {
+        return this._title;
+    } 
+
+    get isCheckedOut() {
+        return this._isCheckedOut;
+    }
+
+    get ratings() {
+        return this._ratings;
+    }
 };
