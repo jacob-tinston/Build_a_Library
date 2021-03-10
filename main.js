62fffbd451e9e7fe562f1fa80fc87d5b502756cd
jacob-tinston <jtinston678@gmail.com>
2021-03-10 18:47:15 +0000

    Fourteenth Task

--- main.js
+++ main.js
@@ -49,3 +49,19 @@ class Book extends Media {
         return this._pages;
     }
 };
+
+class Movie extends Media {
+    constructor(title, director, runtime) {
+        super(title);
+        this._director = director;
+        this._runtime = runtime;
+    }
+
+    get director() {
+        return this._director;
+    }
+
+    get runtime() {
+        return this._runtime;
+    }
+}
