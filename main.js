e9842695fc70dda0a7958b2193d1e08df0c9b557
jacob-tinston <jtinston678@gmail.com>
2021-03-10 18:44:07 +0000

    Tasks 9-13

--- main.js
+++ main.js
@@ -33,3 +33,19 @@ class Media {
         this._ratings.push(newRating);
     }
 };
+
+class Book extends Media {
+    constructor(title, author, pages) {
+        super(title);
+        this._author = author;
+        this._pages = pages;
+    }
+
+    get author() {
+        return this._author;
+    }
+
+    get pages() {
+        return this._pages;
+    }
+};
